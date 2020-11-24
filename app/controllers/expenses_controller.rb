f71330f453d23fc2c836a1a19f02732aa8b624f0
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-11-24 12:35:39 -0500

    reworked the expenses controller, and finished the views

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,24 +2,35 @@ class ExpensesController < ApplicationController
 before_action :set_user
 
   def index
-    @income = User.bookings.price.all(@user)
-    @totalincome = 0
+    current_user.bookings.each do |booking|
+      @income = 0
+      @income += booking.price
+    end
 
-    @income.each { |price| @totalincome += price }
+    @expenses = Expense.all
+
+    @expenses.each do |expense|
+      @total_expenses = 0
+      @total_expenses += expense.amount
+    end
+
+    @final_balance = @income - @total_expenses
+    # @totalincome = 0
+    # @income.each { |price| @totalincome += price }
 
 
 
-    @expenses = Expense.amount.all(@user)
-    @totalexpenses = 0
+    # @expenses = Expense.amount.all(@user)
+    # @totalexpenses = 0
 
-    @expenses.each { |amount| @totalexpenses += amount }
+    # @expenses.each { |amount| @totalexpenses += amount }
 
-    @revenue = @totalincome - @totalexpenses
+    # @revenue = @totalincome - @totalexpenses
 
   end
 
   def new
-  @expense = Expense.new(params[:user_id])
+    @expense = Expense.new(params[:user_id])
   end
 
   def create
