8b4ab781321f8501b1316740bf571a165a51b6a3
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-11-25 11:58:56 -0500

    Added extra route for finances, finances controller and view, all of them displaying, it still needs testing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resource :dashboard, only: [:show], :controller => :dashboard
   resource :calendar, only: [:show], :controller => :calendar
+  resource :finances, only: [:show], :controller => :finances
 
   resources :clients, only: [:index, :show, :new, :create]
   resources :bookings, only: [:new, :create, :destroy]
