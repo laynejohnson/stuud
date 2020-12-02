14aad11117b6e13cd47388b862486b510bbfe06c
Alain Rusnov <alainrusnov@gmail.com>
2020-12-02 10:58:45 -0500

    fixed calendar modal, edit fixed for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :set_user
-  before_action :set_user_clients, only: [:new]
+  before_action :set_user_clients, only: [:new,:edit]
 
   def index
     # start_date = params[:start]
