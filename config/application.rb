f4fe661b7edd418e18bd684fcb9a0bba25caa8d7
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2020-12-03 15:13:35 -0500

    time issue

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Stuud
     # the framework and any gems in your application.
     require 'wicked_pdf'
     config.middleware.use WickedPdf::Middleware
-    config.time_zone = 'Eastern Time (US & Canada)'
+    # config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
